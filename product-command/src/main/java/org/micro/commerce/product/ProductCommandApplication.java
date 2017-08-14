3d4f0a4b030c510045f7343daf7e40f8fe5918fc
mderbent <mderbent@ebay.com>
2017-08-15 02:40:46 +0300

    feature/product-command-service: product command and query project structure

---
+++ product-command/src/main/java/org/micro/commerce/product/ProductCommandApplication.java
@@ -0,0 +1,13 @@
+package org.micro.commerce.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCommandApplication.class, args);
+    }
+
+}
---
+++ product-query/src/main/java/org/micro/commerce/product/ProductQueryApplication.java
@@ -0,0 +1,13 @@
+package org.micro.commerce.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductQueryApplication.class, args);
+    }
+
+}
